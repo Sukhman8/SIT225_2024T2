e7460d458a0bc4336af94eb423f057924af98eae
Sukhman8 <makkarsaab18@gmail.com>
2025-03-18 11:45:44 +0530

    Update Arduino_sketch.ino

--- week-1/Arduino_sketch.ino
+++ week-1/Arduino_sketch.ino
@@ -1,23 +1,23 @@
 void setup() {
-    Serial.begin(9600);  
-    pinMode(LED_BUILTIN, OUTPUT);  
+    Serial.begin(9600);  // Initialize serial communication at a baud rate of 9600
+    pinMode(LED_BUILTIN, OUTPUT);  // Set the built-in LED pin as an output
 }
 
 void loop() {
-    if (Serial.available() > 0) {  
-        int num = Serial.parseInt();  
+    if (Serial.available() > 0) {  // Check if there is incoming data in the serial buffer
+        int num = Serial.parseInt();  // Read an integer from the serial input
 
-        if (num > 0) {
-            for (int i = 0; i < num; i++) {
-                digitalWrite(LED_BUILTIN, HIGH);
-                delay(1000);
-                digitalWrite(LED_BUILTIN, LOW);
-                delay(1000);
+        if (num > 0) {  // If the received number is greater than 0
+            for (int i = 0; i < num; i++) {  // Repeat the blinking process 'num' times
+                digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on
+                delay(1000);  // Wait for 1 second
+                digitalWrite(LED_BUILTIN, LOW);  // Turn the LED off
+                delay(1000);  // Wait for 1 second
             }
 
-            int r = random(2, 6); 
-            Serial.println(r);  
+            int r = random(2, 6); // Generate a random number between 2 and 5 (upper limit is exclusive)
+            Serial.println(r);  // Print the random number to the Serial Monitor
         }
-        delay(100);
+        delay(100);  // Short delay before checking for new serial input
     }
 }
